93c5c1af0af7b202f410159c11a9868173eac95b
Floris <floris@xylex.ai>
2024-06-17 04:47:53 +0200

    feat: Add cronitor and cronitor_runtime modules
    This commit adds the cronitor and cronitor_runtime modules to the project. These modules provide functionality for working with cron expressions and integrating with Cronitor. The cronitor_macro crate is also added as a dependency. This commit includes updates to the Cargo.toml file and the main.rs file.
    
    Closes #123

--- src/lib.rs
+++ src/lib.rs
@@ -91,3 +91,6 @@
 //! ## Verify Cron Expressions
 //! 
 //! If you want to verify a cron expression, you can use this website: [https://crontab.cronhub.io/](https://crontab.cronhub.io/)
+
+pub use cronitor_macro::cronitor;
+pub use cronitor_runtime::cron_runtime;
