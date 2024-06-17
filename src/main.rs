689394ea4c1534832204192617f745e9c9525845
Floris <floris@xylex.ai>
2024-06-17 04:47:00 +0200

    chore: Update cronitor dependency in Cargo.toml and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use cronitor_macro::cronitor;
-use cronitor_runtime::cron_runtime;
+pub use cronitor_macro::cronitor;
+pub use cronitor_runtime::cron_runtime;
+
 
 fn main() {
     println!("Hello, world!");
